233c8b85096bdb1fc7bcc062180a2ba347329304
devjimy912 <devjimy912@gmail.com>
2024-11-16 10:13:49 +0900

    Fix AddData

--- src/components/Pages/AddData.js
+++ src/components/Pages/AddData.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
-const addPost = () => {
+function AddData(){
     let newTitle = 'New Post';
     let newContent = 'Content of new post';
     const newPost = { name: newTitle, occupation: newContent };
 
     fetch('https://6735a7195995834c8a9389c1.mockapi.io/api/v1/posts', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newPost),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newPost),
     })
-      .then(response => response.json())
-      .then(data => {
+    .then(response => response.json())
+    .then(data => {
         setPosts([...posts, data]);
-      })
-      .catch(error => console.error('Error adding post:', error));
-  };
+    })
+    .catch(error => console.error('Error adding post:', error));
+};
+
+export default AddData;
